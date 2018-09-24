7174bbbbcdfde3bc3f118acb6cfb7ad0df068f12
aJonathanSchneider <ajs.private@gmail.com>
2018-09-24 13:05:48 +0200

    card build

---
+++ lib/components/card/card.d.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+export interface ThreePartCardViewProps {
+    className?: string;
+    style?: React.CSSProperties;
+    frontContent: JSX.Element;
+    middleContent: JSX.Element;
+    lastContent: JSX.Element;
+}
+export interface ThreePartCardViewState {
+}
+export default class ThreePartCardView extends React.Component<ThreePartCardViewProps, ThreePartCardViewState> {
+    constructor(props?: any);
+    render(): JSX.Element;
+}
