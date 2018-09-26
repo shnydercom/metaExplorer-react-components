ada5c4ff00c78a77334d475751cf9f441689c96f
aJonathanSchneider <ajs.private@gmail.com>
2018-09-26 12:43:16 +0200

    card component has 1/2/1 flex distribution now and full height/width

--- src/components/card/card.story.tsx
+++ src/components/card/card.story.tsx
@@ -7,6 +7,11 @@ import './card.scss';
 const smallCardStyles = {
 	height: "100px"
 }
+const fourByThreeStyles = {
+	height: "300px",
+	width: "400px",
+	padding: "16px"
+}
 
 const stories = storiesOf('plain components', module);
 stories.addDecorator(withKnobs);
@@ -15,8 +20,64 @@ let simpleCardViewProps: ThreePartCardViewProps = {
 	middleContent: <div>middle content</div>,
 	lastContent: <div>last content</div>
 }
+let emptyfirst: ThreePartCardViewProps = {
+	frontContent: null,
+	middleContent: <div>middle, first null</div>,
+	lastContent: <div>last content</div>
+}
+
+let secondEmpty:  ThreePartCardViewProps = {
+	frontContent: <div>front, 2nd null</div>,
+	middleContent: null,
+	lastContent: <div>last content</div>
+}
+let thirdEmpty: ThreePartCardViewProps = {
+	frontContent: <div>front content</div>,
+	middleContent: <div>middle content, last null</div>,
+	lastContent: null
+}
+let onlyFront: ThreePartCardViewProps = {
+	frontContent: <div>only front</div>,
+	middleContent: null,
+	lastContent: null
+}
+let onlyMiddle: ThreePartCardViewProps = {
+	frontContent: null,
+	middleContent: <div>only middle</div>,
+	lastContent: null
+}
+let onlyLast: ThreePartCardViewProps = {
+	frontContent: null,
+	middleContent: null,
+	lastContent: <div>only last</div>
+}
 stories.add('cards/threepartcardview', () => (
-	<ThreePartCardView style={smallCardStyles} {...simpleCardViewProps}>
-		<div>{text("child content", "child Content")}</div>
-	</ThreePartCardView>
+	<>
+		<ThreePartCardView style={smallCardStyles} {...simpleCardViewProps}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+		<ThreePartCardView style={fourByThreeStyles} {...simpleCardViewProps}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+
+		<ThreePartCardView style={fourByThreeStyles} {...emptyfirst}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+		<ThreePartCardView style={fourByThreeStyles} {...secondEmpty}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+		<ThreePartCardView style={fourByThreeStyles} {...thirdEmpty}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+
+		<ThreePartCardView style={fourByThreeStyles} {...onlyFront}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+		<ThreePartCardView style={fourByThreeStyles} {...onlyMiddle}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+		<ThreePartCardView style={fourByThreeStyles} {...onlyLast}>
+			<div>{text("child content", "child Content")}</div>
+		</ThreePartCardView>
+	</>
 ));
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -19,16 +19,22 @@ export default class ThreePartCardView extends React.Component<ThreePartCardView
 		const { children, className, frontContent, middleContent, lastContent, style } = this.props;
 		return <div style={style} className={className ? className : null}>
 			<div className={"mdc-card card"}>
-				<div className="mdc-card__actions front">
-					{frontContent}
-				</div>
-				<div className="mdc-card__primary-action middle">
-					{middleContent}
-					{children}
-				</div>
-				<div className="mdc-card__actions last">
-					{lastContent}
-				</div>
+				{frontContent ?
+					<div className="mdc-card__actions front">
+						{frontContent}
+					</div>
+					: null}
+				{middleContent ?
+					<div className="mdc-card__primary-action middle">
+						{middleContent}
+						{children}
+					</div>
+					: null}
+				{lastContent ?
+					<div className="mdc-card__actions last">
+						{lastContent}
+					</div>
+					: null}
 			</div>
 		</div>
 	}
