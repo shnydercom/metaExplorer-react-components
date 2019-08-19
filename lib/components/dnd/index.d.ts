89daabcfb8057787db35901f21cf8ad0ea1573ef
aJonathanSchneider <ajs.private@gmail.com>
2019-08-19 11:42:30 +0200

    added TDataType to drag interface

---
+++ lib/components/dnd/index.d.ts
@@ -0,0 +1,5 @@
+export * from './interfaces';
+export * from './drag/dragContainer';
+export * from './drop/dropContainer';
+export * from './move/moveContainer';
+export * from './transit/transitComponent';
--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './components/treeview/treeview';
 export * from './components/hero/hero';
 export * from './components/minitoolbox';
 export * from './components/tabs';
+export * from './components/dnd';
